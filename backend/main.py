a4fb4cc0e45ceb62a62962d4f09904fe60e71a8e
Sachin-haridas <sachincusat123@gmail.com>
2026-05-24 03:07:20 +0800

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173",
+                  "https://ask-base-five.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
